8633b546b000f18e01261fe2f51f1fd94863047c
gyedhujk <joris.porpsg@hotmail.com>
2024-05-12 20:21:00 +0700

    hi commit

--- index.js
+++ index.js
@@ -1,16 +1,2 @@
-function minDepth(root) {
-  if (!root) return 0;
-  const queue = [root];
-  let depth = 1;
-  while (queue.length) {
-    const size = queue.length;
-    for (let i = 0; i < size; i++) {
-      const node = queue.shift();
-      if (!node.left && !node.right) return depth;
-      if (node.left) queue.push(node.left);
-      if (node.right) queue.push(node.right);
-    }
-    depth++;
-  }
-  return depth;
-}
+const currentDate = new Date();
+console.log(currentDate); // Fri 2024-04-05T09:17:00.000Z
